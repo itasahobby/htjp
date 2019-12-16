ee341b8990b91c960705dc8b4d2039bc2979fc89
itasahobby <itasahobby@gmail.com>
2019-12-17 00:29:44 +0100

    Progressed with the structure of the request processor

--- src/Server/SimpleProcessor.java
+++ src/Server/SimpleProcessor.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 public class SimpleProcessor implements  RequestProcessor{
     
     private void errorResponse(){
-        
+        //create error message and stop the communication
     }
     
     private void processState(String stateLine){
@@ -27,11 +27,45 @@ public class SimpleProcessor implements  RequestProcessor{
             errorResponse();
     }
     
+    private void processHeader(String headLine){
+        String[] splitted = headLine.split(" ",2);
+        switch(splitted[0]){
+            case "Date:":
+                // Comprobar que sea la misma hora con margen de un par de segundos
+            case "Server":
+                // Open to filter by Server, not implemented in this simple processor
+            case "Content-Length":
+                // This needs to match the length of the body
+            case "Content-Type":
+                // This needs to match the content of the body
+                break;
+            default:
+                errorResponse();
+        }
+    } 
+    
+    private void processBody(String bodyLine){
+        //process body here
+    }
+    
     @Override
     public void process(BufferedReader in) {
         try {
             String stateLine = in.readLine();
             processState(stateLine);
+            
+            String headLine;
+            do{
+                headLine = in.readLine();
+                processHeader(headLine);
+            }while(!headLine.isEmpty());
+            
+            in.readLine();
+            String bodyLine;
+            do{
+                bodyLine = in.readLine();
+                processBody(bodyLine);
+            }while(!headLine.isEmpty());
         } catch (IOException ex) {
             Logger.getLogger(SimpleProcessor.class.getName()).log(Level.SEVERE, null, ex);
         }
